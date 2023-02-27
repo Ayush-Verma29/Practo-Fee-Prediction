85b0627493d8c1fef847f3b32782a1aed834995e
Ayush Kumar Verma <123511060+Ayush-Verma29@users.noreply.github.com>
2023-02-27 11:02:03 +0530

    all

---
+++ README.md
@@ -0,0 +1,62 @@
+Doctor fee prediction 
+
+## Start Machine Learning project.
+
+### Software and account Requirement.
+
+1. [Github Account](https://github.com)
+2. [VS Code IDE](https://code.visualstudio.com/download)
+3. [GIT cli](https://git-scm.com/downloads)
+4. [GIT Documentation](https://git-scm.com/docs/gittutorial)
+
+
+Creating conda environment
+```
+conda create -p venv python==3.7 -y
+```
+```
+conda activate venv/
+```
+OR 
+```
+conda activate venv
+```
+
+```
+pip install -r requirements.txt
+```
+
+To Add files to git
+```
+git add .
+```
+
+OR
+```
+git add <file_name>
+```
+
+> Note: To ignore file or folder from git we can write name of file/folder in .gitignore file
+To check the git status 
+```
+git status
+```
+To check all version maintained by git
+```
+git log
+```
+
+To create version/commit all changes by git
+```
+git commit -m "message"
+```
+
+To send version/changes to github
+```
+git push origin main
+```
+
+To check remote url 
+```
+git remote -v
+```
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv('doctor clean data.csv')
+print(df.columns)
+
+modal=pickle.load(open('pipe.pkl','rb'))
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    City=sorted(df.City.unique())
+    return render_template('index.html',City=City)
+
+@app.route('/predict',methods=['post'])
+
+def predict_fee():
+   DP_Score= int(request.form.get('DP Score'))
+   NPV_Value=int(request.form.get('NPV Value'))
+   City=request.form.get('City')
+   Years_of_Experience=int(request.form.get('Years of Experience'))
+   Speciality=request.form.get('Speciality')
+   Degree_no=int(request.form.get('Degree_no'))
+   Degree_1=request.form.get('Degree_1')
+
+   result=round(modal.predict(np.array([DP_Score,NPV_Value,City,Years_of_Experience,Speciality,Degree_no,Degree_1]).reshape(1,7))[0])
+   result=f'\u20B9{result}'
+   return render_template('index.html',result=result)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8080)
